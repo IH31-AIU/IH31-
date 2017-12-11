86830cdc90cb20f369e7ccb6307bf69c4e0e4d98
Araki <sword.of.the.sky@gmail.com>
2017-12-11 15:51:49 +0900

    見積書作成画面のタイトル変更

--- IH31_CP_Projects/EstimateInput.Designer.cs
+++ IH31_CP_Projects/EstimateInput.Designer.cs
@@ -50,19 +50,18 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label5.Location = new System.Drawing.Point(7, 25);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(9, 31);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(175, 33);
+            this.label5.Size = new System.Drawing.Size(217, 40);
             this.label5.TabIndex = 42;
             this.label5.Text = "見積書作成";
             // 
             // BtMemoSearch
             // 
-            this.BtMemoSearch.Location = new System.Drawing.Point(227, 29);
-            this.BtMemoSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtMemoSearch.Location = new System.Drawing.Point(303, 36);
+            this.BtMemoSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtMemoSearch.Name = "BtMemoSearch";
-            this.BtMemoSearch.Size = new System.Drawing.Size(70, 20);
+            this.BtMemoSearch.Size = new System.Drawing.Size(93, 25);
             this.BtMemoSearch.TabIndex = 52;
             this.BtMemoSearch.Text = "検索画面へ";
             this.BtMemoSearch.UseVisualStyleBackColor = true;
@@ -70,86 +69,81 @@
             // 
             // TbMemo
             // 
-            this.TbMemo.Location = new System.Drawing.Point(121, 31);
-            this.TbMemo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbMemo.Location = new System.Drawing.Point(161, 39);
+            this.TbMemo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TbMemo.Name = "TbMemo";
             this.TbMemo.ReadOnly = true;
-            this.TbMemo.Size = new System.Drawing.Size(88, 19);
+            this.TbMemo.Size = new System.Drawing.Size(116, 22);
             this.TbMemo.TabIndex = 51;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 40);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(67, 50);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.Size = new System.Drawing.Size(0, 15);
             this.label3.TabIndex = 50;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(53, 36);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(71, 45);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 12);
+            this.label6.Size = new System.Drawing.Size(58, 15);
             this.label6.TabIndex = 49;
             this.label6.Text = "メモ番号";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 74);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(71, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 53;
             this.label1.Text = "顧客名";
             // 
             // TbCusName
             // 
-            this.TbCusName.Location = new System.Drawing.Point(121, 72);
-            this.TbCusName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbCusName.Location = new System.Drawing.Point(161, 90);
+            this.TbCusName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TbCusName.Name = "TbCusName";
             this.TbCusName.ReadOnly = true;
-            this.TbCusName.Size = new System.Drawing.Size(88, 19);
+            this.TbCusName.Size = new System.Drawing.Size(116, 22);
             this.TbCusName.TabIndex = 54;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(225, 76);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(300, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(67, 15);
             this.label2.TabIndex = 55;
             this.label2.Text = "担当者名";
             // 
             // TbEmpName
             // 
-            this.TbEmpName.Location = new System.Drawing.Point(296, 74);
-            this.TbEmpName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbEmpName.Location = new System.Drawing.Point(395, 92);
+            this.TbEmpName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TbEmpName.Name = "TbEmpName";
             this.TbEmpName.ReadOnly = true;
-            this.TbEmpName.Size = new System.Drawing.Size(76, 19);
+            this.TbEmpName.Size = new System.Drawing.Size(100, 22);
             this.TbEmpName.TabIndex = 56;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(53, 109);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(71, 136);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.Size = new System.Drawing.Size(67, 15);
             this.label4.TabIndex = 57;
             this.label4.Text = "見積明細";
             // 
             // BtAddEst
             // 
-            this.BtAddEst.Location = new System.Drawing.Point(121, 105);
-            this.BtAddEst.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtAddEst.Location = new System.Drawing.Point(161, 131);
+            this.BtAddEst.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtAddEst.Name = "BtAddEst";
-            this.BtAddEst.Size = new System.Drawing.Size(70, 20);
+            this.BtAddEst.Size = new System.Drawing.Size(93, 25);
             this.BtAddEst.TabIndex = 58;
             this.BtAddEst.Text = "追加画面へ";
             this.BtAddEst.UseVisualStyleBackColor = true;
@@ -157,10 +151,10 @@
             // 
             // BtSubmit
             // 
-            this.BtSubmit.Location = new System.Drawing.Point(797, 510);
-            this.BtSubmit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtSubmit.Location = new System.Drawing.Point(1063, 638);
+            this.BtSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtSubmit.Name = "BtSubmit";
-            this.BtSubmit.Size = new System.Drawing.Size(76, 41);
+            this.BtSubmit.Size = new System.Drawing.Size(101, 51);
             this.BtSubmit.TabIndex = 60;
             this.BtSubmit.Text = "登録";
             this.BtSubmit.UseVisualStyleBackColor = true;
@@ -178,34 +172,36 @@
             this.groupBox1.Controls.Add(this.TbEmpName);
             this.groupBox1.Controls.Add(this.TbCusName);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(37, 62);
+            this.groupBox1.Location = new System.Drawing.Point(49, 78);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(835, 135);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1113, 169);
             this.groupBox1.TabIndex = 61;
             this.groupBox1.TabStop = false;
             // 
             // DvEst
             // 
             this.DvEst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DvEst.Location = new System.Drawing.Point(37, 202);
-            this.DvEst.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DvEst.Location = new System.Drawing.Point(49, 252);
+            this.DvEst.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DvEst.Name = "DvEst";
             this.DvEst.RowTemplate.Height = 27;
-            this.DvEst.Size = new System.Drawing.Size(836, 304);
+            this.DvEst.Size = new System.Drawing.Size(1115, 380);
             this.DvEst.TabIndex = 62;
             // 
             // EstimateInput
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 562);
+            this.ClientSize = new System.Drawing.Size(1179, 702);
             this.Controls.Add(this.DvEst);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.BtSubmit);
             this.Controls.Add(this.label5);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "EstimateInput";
-            this.Text = "EstimateInput";
+            this.Text = "見積書作成画面";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DvEst)).EndInit();
